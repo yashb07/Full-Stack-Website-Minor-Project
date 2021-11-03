9496d195d1aa0970dc042fab64f27b87e359b94e
Yash Belsare <yashrbelsare@gmail.com>
2021-11-03 19:10:22 +0530

    add: comment section basic + database updation

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -103,7 +103,19 @@ class Post {
 						$last_name = $user_row['last_name'];
 						$profile_pic = $user_row['profile_pic'];
 
+						?>
+						
+						<script>
+							function toggle<?php echo $id;?>() {
+								var element = document.getElementById("toggleComment<?php echo $id;?>");
+								if (element.style.display == "block")
+									element.style.display = "none";
+								else
+									element.style.display = "block";
+							}
+						</script>
 
+						<?php
 						//Timeframe
 						$date_time_now = date("Y-m-d H:i:s");
 						$start_date = new DateTime($date_time); //Time of post
@@ -179,7 +191,7 @@ class Post {
 						}
 
 						$str .= "
-						<div class='status-post'>
+						<div class='status-post' onClick='javascript:toggle$id()'>
 									<div class='post-profile-pic'>
 										<img src='$profile_pic'>
 									</div>
@@ -197,6 +209,11 @@ class Post {
 											</div>
 										</div>
 									</div>
+								</div>
+								<div class='post_comment' id='toggleComment$id' style='display: none;'>
+									<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'>
+
+									</iframe>
 								</div>
 								<hr>
 								";
